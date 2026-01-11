0377abdb239833930bbed406cbc975ea893e8af1
chima <chimajunior14@gmail.com>
2026-01-11 23:52:35 +0000

    search icon added to search bar

--- frontend/src/components/Common/SearchBar.jsx
+++ frontend/src/components/Common/SearchBar.jsx
@@ -6,11 +6,15 @@ const SearchBar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleSearchToggle = () => {
-    setIsOpen(!IsOpen);
+    setIsOpen(!isOpen);
   };
 
   return (
-    <div className={`flex items-center justify-center w-full transition-all duration-300 ${isOpen ? "absolute top-0 left-0 w-full bg-white j-24 z-50" : "`w-auto"  } `}>
+    <div
+      className={`flex items-center justify-center w-full transition-all duration-300 ${
+        isOpen ? "absolute top-0 left-0 w-full bg-white j-24 z-50" : "`w-auto"
+      } `}
+    >
       {isOpen ? (
         <form className="relative flex items-center justify-center w-full">
           <div className="relative w-1/2">
@@ -20,6 +24,13 @@ const SearchBar = () => {
               value={searchTerm}
               className="bg-gray-100 px-4 py-2 pl-2 pr-12 rounded-lg focus:outline-none w-full placeholder:text-gray-700"
             />
+            {/* search icon */}
+            <button
+              type="submit"
+              className="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-600 hover:text-gray-800"
+            >
+              <HiMagnifyingGlass className="h-6 w-6" />
+            </button>
           </div>
         </form>
       ) : (
