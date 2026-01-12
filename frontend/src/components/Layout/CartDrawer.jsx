baac863c1c74b17b9ee05858287758d32dedc568
chima <chimajunior14@gmail.com>
2026-01-12 01:02:29 +0000

    adding checkout button fixed at the bottom of carddrawer

--- frontend/src/components/Layout/CartDrawer.jsx
+++ frontend/src/components/Layout/CartDrawer.jsx
@@ -17,11 +17,13 @@ const CartDrawer = ({drawerOpen, toggleCartDrawer}) => {
       <div className="grow p-4 overflow-y-auto">
         <h2 className="text-xl font-semibold mb-4">Your Cart </h2>
 
-     
       </div>
 
-      
-
+      {/* checkout button fixed at the bottom */}
+      <div className="p-4 bg-white sticky bottom-0">
+        <button className="w-full bg-black text-white py-3 rounded-lg font-semibold hover:bg-gray-800 transition">Checkout</button>
+        <p className="text-sm tracking-tighter text-gray-500 mt-2 text-center">Shipping, taxes, and discount codes calculated at checkout </p>
+      </div>
     </div>
   );
 };
