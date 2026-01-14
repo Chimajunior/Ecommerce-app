715d47a8b7f746228e6f7374271656fa10143a42
chima <chimajunior14@gmail.com>
2026-01-14 00:39:13 +0000

    mens collection section added

--- frontend/src/components/Products/GenderCollectionSection.jsx
+++ frontend/src/components/Products/GenderCollectionSection.jsx
@@ -5,7 +5,6 @@ const GenderCollectionSection = () => {
   return (
     <section className="py-6 px-4 lg:px-0">
       <div className="container mx-auto flex flex-col md:flex-row gap-8">
-        
         {/* womens collection */}
         <div className="relative flex-1">
           <img
@@ -14,14 +13,34 @@ const GenderCollectionSection = () => {
             className="w-full h-175 object-cover"
           />
           <div className="absolute bottom-8 left-8 bg-white/90 p-4">
-          <h2 className="text-2xl font-bold text-gray-900 mb-3">
-            Women's Collection
-          </h2>
-          <Link to="/collections/all?gender=Women" className="text-gray-900 underline"> Shop Now </Link>
-
+            <h2 className="text-2xl font-bold text-gray-900 mb-3">
+              Women's Collection
+            </h2>
+            <Link
+              to="/collections/all?gender=Women"
+              className="text-gray-900 underline"
+            >
+              {" "}
+              Shop Now{" "}
+            </Link>
           </div>
         </div>
 
+         {/* mens collection */}
+         <div className="relative flex-1">
+          <img
+            src={mensCollectionImage}
+            alt="Women's Collection"
+            className="w-full h-175 object-cover"
+          />
+          <div className="absolute bottom-8 left-8 bg-white/90 p-4">
+          <h2 className="text-2xl font-bold text-gray-900 mb-3">
+            Men's Collection
+          </h2>
+          <Link to="/collections/all?gender=Men" className="text-gray-900 underline"> Shop Now </Link>
+
+          </div>
+        </div>
       </div>
     </section>
   );
