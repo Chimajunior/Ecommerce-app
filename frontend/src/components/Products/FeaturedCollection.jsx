905e51f16c5bb9e0a9a1205ea12a4b2338794288
chima <chimajunior14@gmail.com>
2026-02-02 16:59:40 +0000

    right side of featured collection added

--- frontend/src/components/Products/FeaturedCollection.jsx
+++ frontend/src/components/Products/FeaturedCollection.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router'
+import featured from "../../assets/featured.webp"
 
 const FeaturedCollection = () => {
   return (
     <section className='py-16 px-4 lg:px-0'>
-        <div className='container mx-auto flex flex-col-reverse lg:flex-row items-center bg-green-50 rounded-3xl'></div>
+        <div className='container mx-auto flex flex-col-reverse lg:flex-row items-center bg-green-50 rounded-3xl'>
         {/* Left content */}
         <div className='lg:w-1/2 p-8 text-center lg:text-left'>
             <h2 className='text-lg font-semibold text-gray-700 mb-2'>
@@ -16,9 +17,15 @@ const FeaturedCollection = () => {
             <p className='text-lg text-gray-600 mb-6'>
                 Discover high-quality, comfortable clothing that effortlessly blends fashion and function. Designed to make you look and feel great every day. 
             </p>
-            <Link to="/collections/all" className="bg-black text-white px06 py-3 rounded-lg text-lg hover:bg-gray-800">Shop Now</Link>
+            <Link to="/collections/all" className="bg-black text-white px-6 py-3 rounded-lg text-lg hover:bg-gray-800">Shop Now</Link>
         
         </div>
+
+        {/* Right content */}
+        <div className='lg:w-1/2'>
+        <img src={featured} alt="featured collection"
+        className='w-full h-full object-cover lg:rounded-tr-3xl lg:rounded-br-3xl' /></div>
+    </div>
     </section>
   )
 }
